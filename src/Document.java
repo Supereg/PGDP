364ab68662e92d0bb31203c9c26dda95cc6df3fb
Supereg <mail@anderl-bauer.de>
2018-11-10 19:35:47 +0100

    Exercise 4.7 1-4

--- src/Document.java
+++ src/Document.java
@@ -6,10 +6,19 @@
  */
 public class Document {
 
+    public static final String[] SUFFICES = {
+            "ab", "al", "ant", "artig", "bar", "chen", "ei", "eln", "en", "end", "ent", "er", "fach", "fikation",
+            "fizieren", "fähig", "gemäß", "gerecht", "haft", "haltig", "heit", "ie", "ieren", "ig", "in", "ion",
+            "iren", "isch", "isieren", "isierung", "isierung", "ist", "ität", "iv", "keit", "kunde", "legen", "legen",
+            "lich", "ling", "logie", "los", "mal", "meter", "mut", "nis", "or", "sam", "schaft", "tum", "ung", "voll",
+            "wert", "würdig"
+    };
+
     private String title;
 
     private String content;
     private String summary;
+    private WordCountsArray wordCounts;
 
     private String language;
 
@@ -39,6 +48,10 @@ public class Document {
 
     public String getSummary() {
         return summary;
+    }
+
+    public WordCountsArray getWordCounts() {
+        return wordCounts;
     }
 
     public String getLanguage() {
@@ -90,6 +103,72 @@ public class Document {
                 ", author=" + author +
                 ", releaseDate=" + releaseDate +
                 '}';
+    }
+
+    private static String[] tokenize(String content) {
+        // assumptions as of the exercise: content has only lower case letters and single spaces
+        int wordCount = Stringulina.countSubstring(content, " ") + 1;
+
+        String[] tokenArray = new String[wordCount];
+        int nextIndex = 0;
+
+        // we append a space so the last word is also copied to the tokenArray and we do not need to make
+        // some special case for it
+        content += " ";
+
+        String currentWord = "";
+        for (int i = 0; i < content.length(); i++) {
+            char c = content.charAt(i);
+
+            if (c == ' ') {
+                tokenArray[nextIndex++] = currentWord;
+                currentWord = "";
+            }
+            else {
+                // string concatenation in loop is still bad. Are we still not allowed to use StringBuilder?
+                //noinspection StringConcatenationInLoop
+                currentWord += c;
+            }
+        }
+
+        return tokenArray;
+    }
+
+    /**
+     * Checks if the last {@code n} characters of {@code w1} and {@code w2} are equal
+     *
+     * @param w1  the first {@code String}
+     * @param w2  the second {@code Srting}
+     * @param n   an {@code int}
+     * @return    {@code true} if the last {@code n} characters of the Strings match.
+     *            If {@code n} is bigger than the length of one of the strings {@code false} is returned
+     */
+    private static boolean sufficesEqual(String w1, String w2, int n) {
+        if (n > w1.length() || n > w2.length())
+            return false;
+
+        int index1 = w1.length() - n;
+        int index2 = w2.length() - n;
+
+        boolean matchesSuffix = true;
+        for (; index1 < w1.length() && index2 < w2.length(); index1++, index2++) {
+            char c1 = w1.charAt(index1);
+            char c2 = w2.charAt(index2);
+
+            if (c1 != c2) {
+                matchesSuffix = false;
+                //System.out.println(c1 + " " +c2 + " do not match");
+                break;
+            }
+        }
+
+        if (matchesSuffix) {
+            matchesSuffix = index1 == w1.length() && index2 == w2.length();
+            // if (!matchesSuffix)
+            //    System.out.println("One of the indices did not finish (" + index1 + "," + index2 + ")");
+        }
+
+        return matchesSuffix;
     }
 
 }
