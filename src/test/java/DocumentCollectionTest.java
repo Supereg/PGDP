56d570adba450c6bcd8bd3666836beeced7c0f74
Supereg <mail@anderl-bauer.de>
2018-11-27 00:18:32 +0100

    Adding Test class for DocumentCollection

---
+++ src/test/java/DocumentCollectionTest.java
@@ -0,0 +1,106 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Andi on 2018-11-26.
+ */
+public class DocumentCollectionTest {
+
+    private DocumentCollection collection;
+
+    @Before
+    public void before() {
+        collection = new DocumentCollection();
+    }
+
+    @After
+    public void cleanup() {
+        collection = null;
+    }
+
+    @Test
+    public void testList() {
+        Document first = createDocument("testa", "tolles buch a"); // 1
+        Document second = createDocument("testb", "tolles buch b"); // 0
+        Document third = createDocument("testc", "tolles buch c"); // 2
+        collection.appendDocument(first);
+        assertEquals(collection.getLastDocument(), collection.getFirstDocument());
+        collection.prependDocument(second);
+        collection.appendDocument(third);
+
+
+        assertTrue("Collection is strangely empty!", !collection.isEmpty());
+        assertEquals("Didn't match size", 3, collection.numDocuments());
+
+        assertTrue("Collection didn't contain first document", collection.contains(first));
+        assertTrue("Collection didn't contain second document", collection.contains(second));
+        assertTrue("Collection didn't contain third document", collection.contains(third));
+
+        assertTrue("First element did not match", second.equals(collection.getFirstDocument()));
+        assertTrue("Last element did not match", third.equals(collection.getLastDocument()));
+
+        assertTrue(second.equals(collection.get(0)));
+        assertTrue(first.equals(collection.get(1)));
+        assertTrue(third.equals(collection.get(2)));
+
+        assertEquals(0, collection.indexOf(second));
+        assertEquals(1, collection.indexOf(first));
+        assertEquals(2, collection.indexOf(third));
+
+        collection.removeLastDocument();
+        assertEquals("Size was illegal after last document was removed", 2, collection.numDocuments());
+
+
+        collection.appendDocument(third);
+        collection.remove(2);
+        assertEquals("Size was illegal after document with index 2 was removed", 2, collection.numDocuments());
+        assertFalse(collection.contains(third));
+
+        collection.appendDocument(third);
+
+        assertEquals(3, collection.numDocuments());
+        collection.remove(1);
+        assertEquals("Size was illegal after document with index 1 was removed", 2, collection.numDocuments());
+        assertFalse(collection.contains(first));
+        assertTrue(collection.contains(third));
+        assertTrue(collection.contains(second));
+
+
+        collection.removeFirstDocument();
+        collection.prependDocument(second);
+        collection.prependDocument(first);
+
+
+        collection.remove(0);
+        assertEquals("Size was illegal after document with index 0 was removed", 2, collection.numDocuments());
+        assertFalse(collection.contains(first));
+        assertTrue(collection.contains(second));
+        assertTrue(collection.contains(third));
+
+        collection.removeFirstDocument();
+        assertEquals(1, collection.numDocuments());
+        assertEquals(third, collection.getFirstDocument());
+        assertEquals(third, collection.getLastDocument());
+
+        collection.removeLastDocument();
+        assertEquals(0, collection.numDocuments());
+        assertNull(collection.getFirstDocument());
+        assertNull(collection.getLastDocument());
+
+
+        collection.appendDocument(third);
+
+        collection.remove(0);
+        assertEquals(0, collection.numDocuments());
+        assertNull(collection.getFirstDocument());
+        assertNull(collection.getLastDocument());
+    }
+
+    private static Document createDocument(String titel, String content) {
+        return new Document(titel, "de", "test", new Date(), null, content);
+    }
+
+}
